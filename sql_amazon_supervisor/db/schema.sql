1ef8f98c4f9f510d9e19fe402ddc2f917c3b27e4
Bruno Marques <bmarques8989@gmail.com>
2019-07-24 23:39:49 -0400

    node amazon final

---
+++ sql_amazon_supervisor/db/schema.sql
@@ -0,0 +1,11 @@
+USE sql_amazon;
+
+CREATE TABLE departments (
+    department_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(255),
+    over_head_costs INTEGER DEFAULT 0,
+    product_sales INTEGER DEFAULT 0,
+    total_profit INTEGER DEFAULT 0,
+    PRIMARY KEY (department_id)
+);
+
---
+++ stockit.js
@@ -0,0 +1,45 @@
+//require node packages
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+//connect to mysql database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "sql_amazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connect as id " + connection.threadId);
+    addProducts();
+    afterConnection()
+})
+
+//insert info for 10 random 'mock' items into database
+function addProducts() {
+    console.log("Inserting new product..\.n");
+    let query = connection.query(
+        "INSERT INTO products SET?",
+        {
+            product_name: 'microwave',
+            department_name: 'home and kitchen',
+            price: 30,
+            stock_quantity: 10
+        },
+        function(err,res) {
+            console.log(res.affectedRows + " product inserted!\n");
+            //addProducts();
+        }
+    );
+    console.log(query.sqp);
+}
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    })
+}
